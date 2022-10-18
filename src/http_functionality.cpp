ed6897c7d004de4aa23700458d02a7fa63a02f5f
Nikolaj <nikolajweinell@hotmail.com>
2022-10-18 14:11:52 +0200

    Add string

--- src/http_functionality.cpp
+++ src/http_functionality.cpp
@@ -1,3 +1,10 @@
 #include "http_functionality.h"
+#include <string>
+
+
+const int    HTTP_PORT   = 80;
+const std::string HTTP_METHOD = "GET"; // or "POST"
+const char   HOST_NAME[] = "google.com"; // hostname of web server:
+const std::string PATH_NAME   = "";
 
 
